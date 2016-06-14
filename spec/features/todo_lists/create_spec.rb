1c1e7924b82732ff2f14d03bb0610ad41e3973d3
Katie Gilbert <ktagilbert@gmail.com>
2016-06-14 16:02:52 -0700

    Add validation for description presence

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -30,4 +30,20 @@ describe "Creating todo lists" do
     visit "todo_lists"
     expect(page).to_not have_content("The thing that is saving me from my own anxiety.")
   end
+
+  it "displays an error when the todo list has no description" do
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page). to have_content("New Todo List")
+
+    fill_in "Title", with: "My todo list"
+    fill_in "Description", with: ""
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "todo_lists"
+    expect(page).to_not have_content("My todo list")
+  end
 end
